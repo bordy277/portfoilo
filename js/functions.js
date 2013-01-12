06938149790878ff20f8fe95cc6df1309b016148
Kirk Steineck <kirk@socialsamba.com>
2013-01-12 12:08:02 -0600

    all  kinds of new stuff

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,23 @@
-$(document).ready(function(){
+$('document').ready(function(){
+
+	$('.content-image img').animate({'marginLeft': 0}, 200);
+    $('.nav-item').click(function(){
+
+    	$('.nav-item').removeClass('active');
+	    $(this).addClass('active');
+
+		var name = $(this).attr('name');
+
+		function contentSwitch() {
+	      $('#area').animate({'opacity': 0}, 300, function(){
+	        $(this).load('ajax/' + name + '.html', function(){
+	          $(this).animate({'opacity': [1, 'swing']}, 300);
+	        });
+	      }); 
+	    } 
+
+	    contentSwitch();
+
+	});
+
 });
---
+++ js/images.js
@@ -0,0 +1,114 @@
+var count = $('.content-image *').length;
+
+var current = 1;
+
+$('.content-image img').animate({'marginLeft': 0}, 200);
+
+if (count<2) 
+  {
+    $('.count').hide();
+  }
+
+$(".total").html(count);
+
+$('.current').html(current);
+
+function nextDriver() {
+  $('.current').html(current);
+
+  if (current>1) 
+    {
+      $('.prev').removeClass('no');
+      $('.prev').addClass('yes');
+    }
+  else 
+    {
+      $('.prev').addClass('no'); 
+      $('.prev').removeClass('yes');  
+    }
+
+  if (count<=$('.current').html()) 
+    {
+      $('.next').addClass('no');
+      $('.next').removeClass('yes');
+    }
+  else
+    {
+      $('.next').removeClass('no');
+      $('.next').addClass('yes');
+    }
+  };
+
+$('.next').click(function(){
+  
+  if ($(this).attr('class') == 'next yes')
+    {
+      $('.slider').animate({'marginTop': ['-=500', 'swing']}, 300);
+
+      current++;
+
+      nextDriver();
+    }
+  else 
+    {
+
+    }
+
+});
+
+
+$('.prev').click(function(){
+
+  if ($(this).attr('class') == 'prev yes')
+    {
+      $('.slider').animate({'marginTop': ['+=500', 'swing']}, 300);
+
+      current--;
+
+      nextDriver();
+    }
+  else
+    {
+
+    }
+
+});
+
+function checkKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '39') 
+      {
+        $('.next').trigger('click');
+        return false;
+      }
+    else if (e.keyCode == '40') 
+      {
+        $('.next').trigger('click');
+        return false;
+      }
+    else if (e.keyCode == '37') 
+      {
+        $('.prev').trigger('click');
+        return false;
+      }
+    else if (e.keyCode == '38') 
+      {
+        $('.prev').trigger('click');
+        return false;
+      }
+    else 
+      {
+
+      }
+
+}
+
+document.onkeydown = checkKey;
+
+
+
+
+
+
+

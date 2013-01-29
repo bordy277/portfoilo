ea53ce3a05aeb3c1a8a4e4be2fb9f07531077eea
Kirk Steineck <kirk@socialsamba.com>
2013-01-29 15:57:44 -0600

    front page

--- public/js/functions.js
+++ public/js/functions.js
@@ -29,5 +29,25 @@ function initializeFunction() {
 
 		});
 
+		var theImages = new Array('<img id="first" src="/images/cover-1.jpg" />', '<img id="first" src="/images/cover-2.jpg" />', '<img id="first" src="/images/cover-3.jpg" />')
+        
+        var i = 0;
+      
+
+	    function thing() {
+            $('#front-images').animate({'opacity': [0, 'swing']}, 100, function(){
+				if (i== theImages.length){
+					i = 0; 
+				}
+                $('#front-images').html(theImages[i++]);
+            });
+            $('#front-images').animate({'opacity': 1}, 250)
+        }
+	    function change() {
+	        setInterval(thing, 5000);
+	    }
+
+	    change();
+
 	});
 }
